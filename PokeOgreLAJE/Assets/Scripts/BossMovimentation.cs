509e2ec25dd1b752804d6da37342114ecb3f1efa
Pedro Marangon <pedrommarangon@gmail.com>
2020-07-08 11:16:03 -0300

    a

---
+++ PokeOgreLAJE/Assets/Scripts/BossMovimentation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class BossMovimentation : MonoBehaviour
+{
+
+    public Transform playerChasing;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(playerChasing.position);
+    }
+}
---
+++ PokeOgreLAJE/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Camera cam;
+
+    public LayerMask mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100, mask))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
